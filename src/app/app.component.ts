a0c2ac4d3c1a465a8650a139f27db73942b334f0
ipaschenko <ivanko@paschenko.gmail.com>
2018-10-22 18:08:04 +0300

    test drop lists

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,49 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  items = [
+  list1 = [
     'Item 0',
     'Item 1',
     'Item 2',
     'Item 3',
     'Item 4',
+  ];
+
+  list2 = [
     'Item 5',
     'Item 6',
     'Item 7',
-  ]
+  ];
+
+  todo1 = [
+    'Get to work',
+    'Pick up groceries',
+    'Go home',
+    'Fall asleep'
+  ];
+
+  done2 = [
+    'Get up',
+    'Brush teeth',
+    'Take a shower',
+    'Check e-mail',
+    'Walk dog'
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
 }
